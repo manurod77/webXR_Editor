0aace5450c5c62d1936600220a521a41994e08ca
manurod77 <mephiskaphelles@gmail.com>
2025-05-10 22:49:00 -0500

    c1

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2228230.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 225897433, 225897434, 225897407, 225897436, 225897437, 225897438, 225897439, 225897440, 225897442, 225897443, 225897444, 225897446, 225897447, 225897445 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/225897448/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/225897406/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/225897405/1/ammo.js', 'preload' : true},
+];
